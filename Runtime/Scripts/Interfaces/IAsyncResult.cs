1a47c03e4c6f2d841857ba6776dabf2be3311e10
AXit <107571396+ngocphat03@users.noreply.github.com>
2024-12-14 14:41:37 +0700

    update: IAsyncResult

--- Runtime/Scripts/Interfaces/IAsyncResult.cs
+++ Runtime/Scripts/Interfaces/IAsyncResult.cs
@@ -10,7 +10,20 @@
 #if UNITASK
         public Cysharp.Threading.Tasks.UniTask<T> ToUniTask();
 #else
-        public System.Collections.IEnumerator ToEnumerator();
+        public System.Collections.IEnumerator ToCoroutine(System.Action<T> onComplete = null)
+        {
+            return ToCoroutineInternal(this, onComplete);
+        }
+
+        private static System.Collections.IEnumerator ToCoroutineInternal(IAsyncResult<T> asyncResult, System.Action<T> onComplete = null)
+        {
+            while (!asyncResult.IsCompleted)
+            {
+                yield return null;
+            }
+
+            onComplete?.Invoke(asyncResult.GetResult());
+        }
 #endif
     }
 }
